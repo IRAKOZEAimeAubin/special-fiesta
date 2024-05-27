4692a6ccd159f80f118230487da85d26ad9d3c45
Irakoze Aimé Aubin <irakozeaimeaubin@gmail.com>
2024-05-27 17:53:20 +0200

    generics

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,12 @@ const log = console.log
 const numbers = [1, 2, 3, 4]
 const [a, b, ...c] = numbers
 log(a, b, c)
+
+// Generics
+
+function identity<T>(arr: T[]): T {
+    let index = Math.floor( Math.random() * arr.length )
+    return arr[index]
+}
+
+const val = identity([1, 2, 3, 'Hello'])
